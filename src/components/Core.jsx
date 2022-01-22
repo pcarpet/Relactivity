6dcffcba662377a33220772f55b74827bafb47c6
Pierre MAC <p.carpentier.mail@gmail.com>
2022-01-22 10:53:35 +0100

    ajout et suppression de trips

--- src/components/Core.jsx
+++ src/components/Core.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import './core.scss'
 import firebase from "../firebase.js";
 import moment from "moment";
+import TripList from './TripList';
 import Periode from './Periode';
 import ListEtape from './time/ListEtape';
 import Carte from './space/Carte';
 import {Row, Col} from "antd";
 
 
+
 const db = firebase.database();
 
 //const trip = "lombardie";
 //const trip = "cotedor";
-const trip = "fresnoy";
+
 
 class Core extends React.Component {
 
@@ -20,6 +22,8 @@ class Core extends React.Component {
     super(props);
     
     this.state = {
+      trips : [],
+      selectedTrip: null,
       activities: [],
       defaultPickerValue : [moment("2021-12-23","YYYY-MM-DD"), moment("2021-12-27","YYYY-MM-DD")],
     };
@@ -31,22 +35,43 @@ class Core extends React.Component {
     this.refreshActivities = this.refreshActivities.bind(this);
     this.getDefaultPickerValue = this.getDefaultPickerValue.bind(this);
     this.updateListOfDays = this.updateListOfDays.bind(this);
+    this.handleTripSelection = this.handleTripSelection.bind(this);
+    this.createNewTrip = this.createNewTrip.bind(this);
+    this.deleteTrip = this.deleteTrip.bind(this);
 
   }
 
 
   componentDidMount() {
-    this.loadActivitiesFromDb();
-
+    this.loadTripsFromDb(); 
   }
+  
+  loadTripsFromDb(){
+    
+    let tripsFromDb = [];
+    db.ref("activities/pca").get().then((snapshot) => {
+      if (snapshot.exists()) {
+        snapshot.forEach(activity => {
+          tripsFromDb.push(activity.key);
+        });
+      }
+      
+      console.log(tripsFromDb);
 
-
+      this.setState({trips:tripsFromDb});
+      this.setState({selectedTrip:tripsFromDb[0]});
+      
+      this.loadActivitiesFromDb(tripsFromDb[0]);
+      
+    });
+  }
+  
   //Chargement des donnée de la base
-  loadActivitiesFromDb(){
+  loadActivitiesFromDb(tripKey){
     
-  //  console.log("Chargement des données de la base");
+    //  console.log("Chargement des données de la base");
     
-    db.ref("activities/pca/" + trip).get().then((snapshot) => {
+    db.ref("activities/pca/" + tripKey).get().then((snapshot) => {
       if (snapshot.exists()) {
         
         var activitiesConverted = [];
@@ -77,67 +102,67 @@ class Core extends React.Component {
   getDefaultPickerValue(){
     var allDates = [];
     allDates = this.state.activities.map((act) => act.date);
-
+    
     const maxDate = moment.max(allDates);
     const minDate = moment.min(allDates);
     
     const range = [minDate, maxDate];
-
+    
     this.setState({defaultPickerValue: range});
   }
-
+  
   // On s'assure que la liste contient des activités pour les jours de la période (création d'activités fictives)
   // On va supprimmer toutes les activités liés au jours hors de la période !!!
   updateListOfDays(dateRange){
-
-      const debut = dateRange[0];
-      const fin = dateRange[1];
-
-      console.log(debut);
-
-      var day = debut.clone();
-      var periodeList = [];
-
-      while(day.isSameOrBefore(fin)){
-        periodeList.push(day.clone());
-        day.add(1, 'days');
+    
+    const debut = dateRange[0];
+    const fin = dateRange[1];
+    
+    console.log(debut);
+    
+    var day = debut.clone();
+    var periodeList = [];
+    
+    while(day.isSameOrBefore(fin)){
+      periodeList.push(day.clone());
+      day.add(1, 'days');
+    }
+    
+    console.log(periodeList);
+    
+    //Ajout des activités pour les jours manquant (activité fictive)
+    var newDates = [];
+    for(const d of periodeList){
+      if(this.state.activities.find((activities) => activities.date.isSame(d, 'day'))===undefined){
+        newDates.push(d);
       }
-      
-      console.log(periodeList);
-      
-      //Ajout des activité pour les jours manquant (activité fictive)
-      var newDates = [];
-      for(const d of periodeList){
-        if(this.state.activities.find((activities) => activities.date.isSame(d, 'day'))===undefined){
-          newDates.push(d);
-        }
+    }
+    
+    console.log(newDates);
+    
+    for(const nd of newDates){
+      var dayActivity = {
+        key: 0,
+        activityType : 'day',
+        date: nd,
+        heure: null,
+        nomEtape: null,
+        selected: false,
+      };
+      this.addEtape(dayActivity);
+    }
+    
+    
+    //Suppression de toutes les activités hors période !!!! WARNING : ca peut tout niquer en 2 2 !!!!
+    var activitiesKeysToDelete = [];
+    for(const act of this.state.activities){
+      if(periodeList.find((d) => d.isSame(act.date, 'day'))===undefined){
+        activitiesKeysToDelete.push(act.key)
       }
-      
-      console.log(newDates);
-      
-      for(const nd of newDates){
-        var dayActivity = {
-          key: 0,
-          activityType : 'day',
-          date: nd,
-          heure: null,
-          nomEtape: null,
-          selected: false,
-        };
-        this.addEtape(dayActivity);
-      }
-
-
-      //Suppression de toutes les activités hors période !!!! WARNING : ca peut tout niquer en 2 2 !!!!
-      var activitiesKeysToDelete = [];
-      for(const act of this.state.activities){
-          if(periodeList.find((d) => d.isSame(act.date, 'day'))===undefined){
-            activitiesKeysToDelete.push(act.key)
-          }
-        }
-        this.deleteActivities(activitiesKeysToDelete);
-
-
+    }
+    this.deleteActivities(activitiesKeysToDelete);
+    
+    
   }
   
   /* Ajoute ou met à jour l'étape remontée par le composant Finder à la liste*/
@@ -156,78 +181,68 @@ class Core extends React.Component {
     }
     //on récupére la liste pour la modifier
     var listLocal = this.state.activities;
-
+    
     //sauvegarde de l'étape en base. Si la clef est 0, c'est une nouvelle étape
     if(etape.key === 0){
-      const activitiesRef  = db.ref("activities/pca/" + trip);
+      const activitiesRef  = db.ref("activities/pca/" + this.state.selectedTrip);
       const newEntry = activitiesRef.push(activityForDb);
       etape.key = newEntry.key;
       
       //Ajout de l'étape dans la liste avec la nouvelle clef
       listLocal.push(etape);
     }else{ //mmise à jour de l'étape en base
-      db.ref("activities/pca/" + trip).child(etape.key).update(activityForDb)
-  
+      db.ref("activities/pca/" + this.state.selectedTrip).child(etape.key).update(activityForDb)
+      
       //Mise à jour de l'oject dans la liste
       let activityToUpdate = listLocal.find((act) => etape.key === act.key);    
       Object.assign(activityToUpdate, etape)
     }
-
-  
+    
+    
     this.refreshActivities(listLocal);
-
+    
     //On centre la carte sur la nouvelle étape si ce n'est pas une day activity
     if(etape.activityType !== 'day')
-      this.selectEtape(etape.key);
-
+    this.selectEtape(etape.key);
+    
   }
-
+  
   deleteActivity(key){
     console.log("Supression de l'étape key : " + key);
     var listLocal = this.state.activities;
-    //var startActivity = null;
-    //On supprime la direction qui meme à cette étape si elle existe
-   // const rank = listLocal.find((etape) => etape.key === key).rank;
-    //if(rank>1){
-    //  startActivity = listLocal.find((etape) => etape.rank === rank-1);
-    //  startActivity.directionsResult = null;
-    //}
-
+      
     //On filtre la liste d'activité pour retirer la key de l'activité à supprimer
     listLocal = listLocal.filter(e => e.key !== key);
-    
+      
     //Supression en base
-    db.ref("activities/pca/" + trip).child(key).remove();
-    // if(startActivity !== null){
-    //   db.ref("activities/pca/" + trip).child(startActivity.key).update({directionsResult : null});
-    // }
-
+    db.ref("activities/pca/" + this.state.selectedTrip).child(key).remove();
+     
     //Mise à jour du state
     this.refreshActivities(listLocal);
   }
-
+      
   //Obligé de faire une fonction multiple car le setState de la liste des activité dans le refresh ne la met pas à jour et les élements supprimés reviennent
   deleteActivities(keys){
     console.log("Supression des étape key : " + keys);
     var listLocal = this.state.activities;
-  
+    
     //On filtre la liste d'activité pour retirer la key de l'activité à supprimer
     listLocal = listLocal.filter(e => !keys.includes(e.key));
-       
+    
     //Supression en base
     for(const key of keys){
-      db.ref("activities/pca/" + trip).child(key).remove();
+      db.ref("activities/pca/" + this.state.selectedTrip).child(key).remove();
     }
-
+    
     //Mise à jour du state
     this.refreshActivities(listLocal);
   }
-
-
+      
+      
   //Retrie les activité par date et set la liste
   refreshActivities(listLocal){
-   
-     //Tri des étapes par chronologie
+    
+    //Tri des étapes par chronologie
     listLocal.sort(function (a, b) {
       if (a.date - b.date === 0) { return a.heure - b.heure;};
       return a.date - b.date;
@@ -237,10 +252,10 @@ class Core extends React.Component {
     for (var i = 0; i < listLocal.length; i++) {
       listLocal[i].rank = i + 1;
     }
-
+    
     console.log(listLocal);
     this.setState({ activities: listLocal });
-
+    
     this.getDefaultPickerValue();
   }
 
@@ -255,25 +270,59 @@ class Core extends React.Component {
         ligne.selected = false;
       }
     }
-
+    
     this.setState({ ListV: selectionList });
-
-    // console.log("onselection " + idEtape);
-    // const activities = this.state.activities;
-    // //TODO : Utiliser un find
-    // for (const etape of activities) {
-    //   //Mise à jour de la position de la carte
-    //   if (etape.key === idEtape) {
-    //     this.setState({ position: [etape.origin.lat, etape.origin.long] });
-    //   }
-    // }
+    
+  }
+      
+  handleTripSelection(selectedTrip){
+    console.log("Chargement des activités pour :" + selectedTrip);
+    this.setState({selectedTrip: selectedTrip});
+    this.loadActivitiesFromDb(selectedTrip);
+  }
+  
+  createNewTrip(newTrip){
+    let trips = this.state.trips;
+    trips.push(newTrip);
+    this.setState({ 
+      selectedTrip: newTrip,
+      trips : trips,
+      activities: [] ,
+    });
+    this.getDefaultPickerValue();
   }
 
+  deleteTrip(){
+    
+    const tripToDelete = this.state.selectedTrip;
 
+    //Supression de la base
+    db.ref("activities/pca/" + tripToDelete).remove();
+    let trips = this.state.trips;
+    trips = trips.filter(t => t !== tripToDelete);
+
+    this.setState({ 
+      trips : trips,
+    });
+
+    if(trips[0] !== undefined)
+      this.handleTripSelection(trips[0]);
+  }
+    
   render() {
-    return (
-      <div className="Core">
+      return (
+        <div className="Core">
         <Row>
+          <Col span={8}>
+            <TripList 
+              selectedTrip={this.state.selectedTrip}
+              handleTripSelection={this.handleTripSelection}
+              trips={this.state.trips}
+              createNewTrip={this.createNewTrip}
+              deleteTrip={this.deleteTrip}
+            />
+          </Col>
+  
           <Col span={8}>
             <Periode  
               defaultPickerValue={this.state.defaultPickerValue} 
